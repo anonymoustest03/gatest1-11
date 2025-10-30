ac821df80ef883be6443d7a44e85c00bb2339e8b
Yuxuan Zhou <32389418+ZhouYuxuan97@users.noreply.github.com>
2025-10-29 20:50:32 -0400

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -47,7 +47,7 @@ object Build : BuildType({
         script {
             id = "simpleRunner"
             scriptContent = """
-                ping 127.0.0.1 -n 10 
+                ping 127.0.0.2 -n 10 
             """.trimIndent()
         }
     }
